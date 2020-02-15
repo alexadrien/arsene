25930570b428bff443a20669ec3fd5feb3763409
acube <alexadriena@sipios.com>
2020-02-15 22:39:31 +0100

    Save

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import './App.css';
 import data from "./data.json";
+import useInterval from '@use-it/interval';
 
 type Video = {
   url: string,
+  name: string,
 };
 
+const videos = data;
+
 function App() {
+  useInterval(() => {}, 30000);
   return (
     <div className="pageWrapper">
-      {data.map((video: Video) => (
-          <div className="playingVideoWrapper">
+      {videos.map((video: Video, index) => (
+          <div className="playingVideoWrapper" key={index}>
             <video muted autoPlay loop>
               <source src={video.url}/>
             </video>
+            <span>{video.name}</span>
           </div>
       ))}
     </div>
