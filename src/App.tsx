84b006165638cb64a88669e71fe13f9e9a962740
acube <alexadriena@sipios.com>
2020-02-16 00:05:30 +0100

    Save

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import data from "./data.json";
-import useInterval from '@use-it/interval';
+import { numberOfVideos } from "./contants";
+
 
 type Video = {
   url: string,
   name: string,
 };
 
-const videos = data;
+const initialVideos: Video[] = data.slice(0, numberOfVideos);
 
 function App() {
-  useInterval(() => {}, 30000);
+  const [videos, setVideos] = useState(initialVideos);
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const randomVideoIndex = Math.floor(Math.random() * videos.length);
+      let newVideos = videos;
+      newVideos[randomVideoIndex] = data[data.length - 1];
+      setVideos(newVideos);
+      setCounter(counter + 1);
+    }, 3000);
+    return () => clearInterval(interval);
+  });
+
   return (
-    <div className="pageWrapper">
-      {videos.map((video: Video, index) => (
-          <div className="playingVideoWrapper" key={index}>
-            <video muted autoPlay loop>
-              <source src={video.url}/>
-            </video>
-            <span>{video.name}</span>
-          </div>
-      ))}
-    </div>
+      <div className="pageWrapper">
+        {videos.map((video: Video, index) => (
+            <div className="playingVideoWrapper" key={index}>
+              <video muted autoPlay loop>
+                <source src={video.url} />
+              </video>
+              <span>{video.name}</span>
+            </div>
+        ))}
+      </div>
   );
 }
 
---
+++ src/contants.ts
@@ -0,0 +1,3 @@
+export const numberOfVideosPerLine = 3;
+export const numberOfVideosPerCol = 2;
+export const numberOfVideos = numberOfVideosPerCol * numberOfVideosPerLine;
